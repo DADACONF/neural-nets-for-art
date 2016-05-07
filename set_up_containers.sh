1e8dff7b930b2ee067d8a558df14dda800735f10
Kyle Miller <kylemiller@fb.com>
2016-05-06 23:07:33 -0700

    fix for if your directory has a space

--- set_up_containers.sh
+++ set_up_containers.sh
@@ -12,7 +12,7 @@ function doDeepDream() {
     -e ITER=${2:-'20'} \
     -e SCALE=${3:-'0.1'} \
     -e MODEL=${4-'inception_4c/output'} \
-    -v $(pwd):/data \
+    -v "$(pwd)":/data \
     -it herval/deepdream  
 }
 alias deep-dream=doDeepDream
@@ -22,14 +22,14 @@ docker pull herval/deepdream
 mkdir frames
 mkdir samples
 alias neural-doodle="docker run \
-  -v $(pwd)/samples:/nd/samples \
-  -v $(pwd)/frames:/nd/frames \
+  -v "$(pwd)"/samples:/nd/samples \
+  -v "$(pwd)"/frames:/nd/frames \
   -it alexjc/neural-doodle"
 docker pull alexjc/neural-doodle
 
 ## Char-nn ##
 mkdir cv
 alias char-rnn="docker run \
-  -v $(pwd)/cv:/home/char-rnn/cv \
+  -v "$(pwd)"/cv:/home/char-rnn/cv \
   -it mbartoli/char-rnn"
 docker pull mbartoli/char-rnn
